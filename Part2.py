483fb01f0ba038385166f35ef314c71b2df8b97e
AKUNO0407 <76884596+AKUNO0407@users.noreply.github.com>
2023-08-10 22:32:28 -0400

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -7,83 +7,6 @@ import pandas as pd
 from hasher import Hasher
 from authenticate import Authenticate
 
-_RELEASE = True
-
-if not _RELEASE:
-    # hashed_passwords = Hasher(['abc', 'def']).generate()
-
-    # Loading config file
-    with open('../config.yaml') as file:
-        config = yaml.load(file, Loader=SafeLoader)
-
-    # Creating the authenticator object
-    authenticator = Authenticate(
-        config['credentials'],
-        config['cookie']['name'], 
-        config['cookie']['key'], 
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-
-    # creating a login widget
-    name, authentication_status, username = authenticator.login('Login', 'main')
-    if authentication_status:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{name}*')
-        st.title('Some content')
-    elif authentication_status is False:
-        st.error('Username/password is incorrect')
-    elif authentication_status is None:
-        st.warning('Please enter your username and password')
-
-    # Creating a password reset widget
-    if authentication_status:
-        try:
-            if authenticator.reset_password(username, 'Reset password'):
-                st.success('Password modified successfully')
-        except Exception as e:
-            st.error(e)
-
-    # Creating a new user registration widget
-    try:
-        if authenticator.register_user('Register user', preauthorization=False):
-            st.success('User registered successfully')
-    except Exception as e:
-        st.error(e)
-
-    # Creating a forgot password widget
-    try:
-        username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
-        if username_forgot_pw:
-            st.success('New password sent securely')
-            # Random password to be transferred to user securely
-        else:
-            st.error('Username not found')
-    except Exception as e:
-        st.error(e)
-
-    # Creating a forgot username widget
-    try:
-        username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
-        if username_forgot_username:
-            st.success('Username sent securely')
-            # Username to be transferred to user securely
-        else:
-            st.error('Email not found')
-    except Exception as e:
-        st.error(e)
-
-    # Creating an update user details widget
-    if authentication_status:
-        try:
-            if authenticator.update_user_details(username, 'Update user details'):
-                st.success('Entries updated successfully')
-        except Exception as e:
-            st.error(e)
-
-    # Saving config file
-    with open('../config.yaml', 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
 
     # Alternatively you may use st.session_state['name'], st.session_state['authentication_status'], 
     # and st.session_state['username'] to access the name, authentication_status, and username. 
@@ -187,26 +110,85 @@ def main():
     #password = st.sidebar.text_input("Password", type='password')
     #login_button = st.sidebar.button("Login")
 
-    if authentication_status == False:
-        st.error('Username/password is incorrect')
-    elif authentication_status == None:
-        st.warning('Please enter your username and password')
-    elif authentication_status:
+    _RELEASE = True
+
+if not _RELEASE:
+    # hashed_passwords = Hasher(['abc', 'def']).generate()
+
+    # Loading config file
+    with open('../config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    # Creating the authenticator object
+    authenticator = Authenticate(
+        config['credentials'],
+        config['cookie']['name'], 
+        config['cookie']['key'], 
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+
+    # creating a login widget
+    name, authentication_status, username = authenticator.login('Login', 'main')
+
+    
+    if authentication_status:
         authenticator.logout('Logout', 'main')
-        st.set_page_config(layout="wide")
-        st.title('Customer Success Dashboard')
         st.write(f'Welcome *{name}*')
-        #st.title('Some content')
+        st.title('Some content')
+    elif authentication_status is False:
+        st.error('Username/password is incorrect')
+    elif authentication_status is None:
+        st.warning('Please enter your username and password')
 
-    #if login_button:
-    #    role = authenticate(username, password)
-    #    if role is None:
-    #        st.sidebar.error("Invalid credentials")
-    #    else:
-    #        st.sidebar.empty()  # Clear the login interface
-    #        st.subheader(f"Welcome, {username} ({role.capitalize()})")
-    #        selected_item = st.sidebar.selectbox("Filter by Payment Status", ['Overall'] + list(data['Payment Status'].unique()))
+    # Creating a password reset widget
+    if authentication_status:
+        try:
+            if authenticator.reset_password(username, 'Reset password'):
+                st.success('Password modified successfully')
+        except Exception as e:
+            st.error(e)
 
+    # Creating a new user registration widget
+    try:
+        if authenticator.register_user('Register user', preauthorization=False):
+            st.success('User registered successfully')
+    except Exception as e:
+        st.error(e)
+
+    # Creating a forgot password widget
+    try:
+        username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+        if username_forgot_pw:
+            st.success('New password sent securely')
+            # Random password to be transferred to user securely
+        else:
+            st.error('Username not found')
+    except Exception as e:
+        st.error(e)
+
+    # Creating a forgot username widget
+    try:
+        username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
+        if username_forgot_username:
+            st.success('Username sent securely')
+            # Username to be transferred to user securely
+        else:
+            st.error('Email not found')
+    except Exception as e:
+        st.error(e)
+
+    # Creating an update user details widget
+    if authentication_status:
+        try:
+            if authenticator.update_user_details(username, 'Update user details'):
+                st.success('Entries updated successfully')
+        except Exception as e:
+            st.error(e)
+
+    # Saving config file
+    with open('../config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
 
         if username == 'admin':
             associate_list = data['Customer Success Associate'].unique().tolist()
