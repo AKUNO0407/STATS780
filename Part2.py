61dd1ab37cb1858eb2a64449e6f3ca29584ed0bb
AKUNO0407 <76884596+AKUNO0407@users.noreply.github.com>
2023-08-10 23:47:08 -0400

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -195,7 +195,7 @@ def main():
             if username == 'admin':
                 st.subheader(f"Welcome, {username}")
                 #associate_list = data['Customer Success Associate'].unique().tolist()
-                #selected_associate = st.selectbox("Select Associate", associate_list, key="admin_select_associate")
+                #selected_associate = st.selectbox("Select Associate", associate_list, key=f"{username}_select_associate")
                 #filtered_data_adm = data[data['Customer Success Associate'] == selected_associate]   
                 filtered_data_adm = data
 
