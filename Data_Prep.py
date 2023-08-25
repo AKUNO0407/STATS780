d0058a0ee677b4a21ba2a6b873dfe78652436a61
AKUNO0407 <76884596+AKUNO0407@users.noreply.github.com>
2023-08-25 09:18:34 -0400

    Update Data_Prep.py

--- Data_Prep.py
+++ Data_Prep.py
@@ -119,8 +119,7 @@ def calculate_health_score(df1):
     for i in range(df1.shape[0]):
         row = df1.iloc[i]
         if row['Total_Orders'] != 0:
-            order_disc_rate = round((int(row['Total_Orders'])  - int(row['Total_Printed']))/int(row['Total_Orders']),2) if
-          row['# Printers'] != 0 else 0
+            order_disc_rate = round((int(row['Total_Orders'])  - int(row['Total_Printed']))/int(row['Total_Orders']),2) if row['# Printers'] != 0 else 0
             cancellation_rate = round(int(row['Total_Cancellation']) / int(row['Total_Orders']) ,2)
             missed_rate = round(int(row['Total_Missed'] ) / int(row['Total_Orders']), 2)
         else:
